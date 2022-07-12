bd820dcebc1ed2f41c7c2de958815bb56b38797a
Nikita Vorkov <vorkov711@gmail.com>
2022-07-13 01:56:57 +0300

    init structure

---
+++ Tests/UnitTests/AppsTests/ServerTests/SomeServerTest.h
@@ -0,0 +1,18 @@
+#ifndef NUMBERACCUMULATOR_APPS_TESTS_SOMESERVERTEST_H
+#define NUMBERACCUMULATOR_APPS_TESTS_SOMESERVERTEST_H
+
+
+
+namespace apps
+{
+namespace tests
+{
+
+TEST(SimpleTest, testTest)
+{
+    ASSERT_TRUE(2+2==4);
+}
+
+}
+}
+#endif
---
+++ Tests/UnitTests/LibsTests/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <gtest/gtest.h>
+
+#include "SomeLibTest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
